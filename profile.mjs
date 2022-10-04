039483f7e366bc948458b328f40d29925a8196bd
Bill Thorp <billt@storj.io>
2022-10-04 17:27:30 -0400

    possible race condition

--- profile.mjs
+++ profile.mjs
@@ -4,14 +4,14 @@ export default async function Profile() {
     let name = process.env.PROFILE || 'PROD';
     try {
         let secretData = {}
-        if (name !== 'LOCAL') {
-            const client = new AWS.SecretsManager({region: "ca-central-1"});
-            const secretName = "arn:aws:secretsmanager:ca-central-1:572229791409:secret:storj-UmHCMy";
-            let result = await client.getSecretValue({SecretId: secretName}).promise();
-            secretData = JSON.parse(result.SecretString);
-        }
-        return Object.assign({STORJ:secretData},{PROFILE:name});
-    } catch(e) {
-        throw new Error('Error accessing secrets: '+e.code || e);
+        // if (name !== 'LOCAL') {
+        //     const client = new AWS.SecretsManager({region: "ca-central-1"});
+        //     const secretName = "arn:aws:secretsmanager:ca-central-1:572229791409:secret:storj-UmHCMy";
+        //     let result = await client.getSecretValue({SecretId: secretName}).promise();
+        //     secretData = JSON.parse(result.SecretString);
+        // }
+        return Object.assign({ STORJ: secretData }, { PROFILE: name });
+    } catch (e) {
+        throw new Error('Error accessing secrets: ' + e.code || e);
     }
 }
